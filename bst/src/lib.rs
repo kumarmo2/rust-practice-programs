8e3a60bcac9f6b629807d2d5cb83492359fe7d5c
kumarmo2 <kr.mohit6794@gmail.com>
2023-06-03 16:43:33 +0530

    added preorder traversal

--- bst/src/lib.rs
+++ bst/src/lib.rs
@@ -41,12 +41,58 @@ where
     pub(crate) head: Option<Box<Node<T>>>,
 }
 
-struct InorderIterator<'tree, T>
+pub struct InorderIterator<'tree, T>
 where
     T: Ord,
 {
     node: Option<&'tree Box<Node<T>>>,
     stack: Vec<&'tree Box<Node<T>>>,
+}
+
+pub struct PreorderIterator<'tree, T>
+where
+    T: Ord,
+{
+    node: Option<&'tree Box<Node<T>>>,
+    stack: Vec<&'tree Box<Node<T>>>,
+}
+
+impl<'tree, T> Iterator for PreorderIterator<'tree, T>
+where
+    T: Ord,
+{
+    type Item = &'tree T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.node.is_none() && self.stack.is_empty() {
+            return None;
+        }
+        let node = match self.node {
+            Some(x) => x,
+            None => self.stack.pop().unwrap(),
+        };
+
+        // loop {
+        // let Some(node) = self.node else {
+        // break;
+        // };
+        if let Some(ref right_child) = node.as_ref().right {
+            self.stack.push(right_child);
+        }
+        let result = node.as_ref().deref();
+        self.node = node.as_ref().left.as_ref();
+        return Some(result);
+        // }
+        // let Some(node) =  self.stack.pop() else {
+        // return None;
+        // };
+        // if let Some(ref right_child) = node.as_ref().right {
+        // self.stack.push(right_child);
+        // }
+        // let result = node.as_ref().deref();
+        // self.node = node.as_ref().left.as_ref();
+        // return Some(result);
+    }
 }
 
 impl<'tree, T> Iterator for InorderIterator<'tree, T>
@@ -121,12 +167,76 @@ where
             stack: Vec::new(),
         }
     }
+    pub fn preorder_iter<'tree>(&'tree self) -> PreorderIterator<'tree, T> {
+        PreorderIterator {
+            node: self.head.as_ref(),
+            stack: Vec::new(),
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    #[test]
+    fn preorder_it_works_for_empty_bst() {
+        let bst: Bst<i32> = Bst::new();
+
+        let mut iter = bst.preorder_iter();
+        assert_eq!(None, iter.next());
+    }
+
+    #[test]
+    fn preorder_it_works_for_one_item_bst() {
+        let mut bst: Bst<i32> = Bst::new();
+        bst.add(2);
+
+        let mut iter = bst.preorder_iter();
+        assert_eq!(2, *iter.next().unwrap());
+        assert_eq!(None, iter.next());
+    }
+
+    #[test]
+    fn preorder_it_works_for_two_items_bst() {
+        let mut bst: Bst<i32> = Bst::new();
+        bst.add(2);
+        bst.add(1);
+
+        let mut iter = bst.preorder_iter();
+        assert_eq!(2, *iter.next().unwrap());
+        assert_eq!(1, *iter.next().unwrap());
+        assert_eq!(None, iter.next());
+
+        let mut bst: Bst<i32> = Bst::new();
+        bst.add(1);
+        bst.add(2);
+
+        let mut iter = bst.preorder_iter();
+        assert_eq!(1, *iter.next().unwrap());
+        assert_eq!(2, *iter.next().unwrap());
+        assert_eq!(None, iter.next());
+    }
+
+    #[test]
+    fn preorder_it_works_for_multiple_item_bst() {
+        let mut bst: Bst<i32> = Bst::new();
+        bst.add(5);
+        bst.add(10);
+        bst.add(3);
+        bst.add(4);
+        bst.add(1);
+        bst.add(2);
+
+        let mut iter = bst.preorder_iter();
+        assert_eq!(5, *iter.next().unwrap());
+        assert_eq!(3, *iter.next().unwrap());
+        assert_eq!(1, *iter.next().unwrap());
+        assert_eq!(2, *iter.next().unwrap());
+        assert_eq!(4, *iter.next().unwrap());
+        assert_eq!(10, *iter.next().unwrap());
+        assert_eq!(None, iter.next());
+    }
     #[test]
     fn it_works_for_empty_bst() {
         let bst: Bst<i32> = Bst::new();
