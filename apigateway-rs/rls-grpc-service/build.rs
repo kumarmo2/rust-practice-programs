2ef0fc1c9a924156311665494455f62a6238d73a
Mohit Kumar <kumarmo2@debian.kumarmo2>
2023-07-03 00:19:39 +0530

    minor protos location change

--- apigateway-rs/rls-grpc-service/build.rs
+++ apigateway-rs/rls-grpc-service/build.rs
@@ -2,6 +2,9 @@ fn main() -> std::io::Result<()> {
     tonic_build::configure()
         .build_server(true)
         .out_dir("../compiled_protos")
-        .compile(&["./envoy/service/ratelimit/v3/rls.proto"], &["."])?;
+        .compile(
+            &["./protos/envoy/service/ratelimit/v3/rls.proto"],
+            &["./protos/"],
+        )?;
     Ok(())
 }
