bcc8912d81d5d2dd888e4ce54e16383558c3a5bb
Mohit Kumar <kumarmo2@debian.kumarmo2>
2023-07-06 08:01:06 +0530

    setup for rate limitibg service done

--- apigateway-rs/rls-grpc-service/src/main.rs
+++ apigateway-rs/rls-grpc-service/src/main.rs
@@ -1,3 +1,4 @@
+#![allow(unused_variables)]
 pub(crate) mod envoy;
 
 pub(crate) mod validate;
@@ -6,10 +7,14 @@ pub(crate) mod xds;
 
 pub(crate) mod udpa;
 
-use envoy::service::ratelimit::v3::{
-    rate_limit_service_server, RateLimitRequest, RateLimitResponse,
+use envoy::{
+    extensions::common::ratelimit::v3::rate_limit_descriptor::Entry,
+    service::ratelimit::v3::{
+        rate_limit_service_server::{self, RateLimitServiceServer},
+        RateLimitRequest, RateLimitResponse,
+    },
 };
-use tonic::Code;
+use tonic::{transport::Server, Code, Response};
 struct RateLimitServiceImpl {}
 
 #[tonic::async_trait]
@@ -17,13 +22,34 @@ impl rate_limit_service_server::RateLimitService for RateLimitServiceImpl {
     async fn should_rate_limit(
         &self,
         request: tonic::Request<RateLimitRequest>,
-    ) -> std::result::Result<tonic::Response<RateLimitResponse>, tonic::Status> {
-        Err(tonic::Status::new(Code::Ok, "not passed"))
+    ) -> std::result::Result<Response<RateLimitResponse>, tonic::Status> {
+        println!("here");
+        let request = request.into_inner();
+        for descriptor in request.descriptors.iter() {
+            for Entry { key, value } in descriptor.entries.iter() {
+                println!("key: {}, value: {}", key, value);
+            }
+        }
+        Ok(Response::new(RateLimitResponse {
+            overall_code: Code::Ok as i32,
+            statuses: vec![],
+            quota: None,
+            response_headers_to_add: vec![],
+            request_headers_to_add: vec![],
+            raw_body: vec![],
+            dynamic_metadata: None,
+        }))
+        // Err(tonic::Status::new(Code::Ok, "not passed"))
         // todo!()
     }
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // let x = envoy::service::ratelimit::v3::RateLimitRequest {};
+    Server::builder()
+        .add_service(RateLimitServiceServer::new(RateLimitServiceImpl {}))
+        .serve("0.0.0.0:9000".parse().unwrap())
+        .await?;
+    Ok(())
 }
